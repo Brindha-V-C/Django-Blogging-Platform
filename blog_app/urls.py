e51ff06f76c5301ca1075881d944b7a3d5c16cdc
Brindha-V-C <brindhavc15@gmail.com>
2026-02-19 15:31:05 +0530

    initialize blog app

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for todo_main project.
+URL configuration for blog_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from todo import urls
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('todo/', include('todo.urls'))
 ]
