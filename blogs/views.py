b55371ef1b15324e18cb36d48187858bae85b180
Brindha-V-C <brindhavc15@gmail.com>
2026-02-21 14:51:29 +0530

    add posts by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Blog, Category
 
-# Create your views here.
+def post_by_category(request, category_id):
+
+    #Fetch posts that belongs to the category_id 
+    posts = Blog.objects.filter(status = "Published", category = category_id)
+
+    #To show custom action when the category does not exists
+    try:
+        category_name = Category.objects.get(pk=category_id)
+    except:
+        #Redirect to home page when category does not exist
+        return redirect('home')
+    
+    context = {
+        'posts': posts,
+        'category_name': category_name
+    }
+
+    return render(request, 'post_by_category.html', context)
