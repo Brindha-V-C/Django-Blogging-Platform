f73c3cf640346b8c044e4c43aa39446e7022e1d6
Brindha-V-C <brindhavc15@gmail.com>
2026-02-26 21:33:31 +0530

    add sign-up

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls'), name='category'),
-    path('<slug:slug>/', BlogViews.single_blog, name='single_blog'),
-    path('blogs/search/',BlogViews.search, name='search')
+    path('blogs/<slug:slug>/', BlogViews.single_blog, name='single_blog'),
+    path('blogs/search/',BlogViews.search, name='search'),
+    path('register/', views.register, name='register')
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
