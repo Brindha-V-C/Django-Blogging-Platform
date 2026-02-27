fd7b9fc6595d385bbe3a6660a335b94c56835b46
Brindha-V-C <brindhavc15@gmail.com>
2026-02-27 16:02:06 +0530

    Dashboard for editors

--- blog_app/views.py
+++ blog_app/views.py
@@ -48,7 +48,7 @@ def login(request):
             if user is not None:
                 auth.login(request, user)
             
-            return redirect('home')
+            return redirect('dashboard')
             
     form = AuthenticationForm()
 
